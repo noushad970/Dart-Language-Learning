bcd6619f6fe4b23dd5dcd082f9ed24b0ea15d924
MD. NOUSHAD BHUIYAN <142130428+noushad970@users.noreply.github.com>
2023-08-25 04:52:35 +0600

    Add files via upload

---
+++ is, is! operator.dart
@@ -0,0 +1,11 @@
+void main() {
+  // is operator
+//this operator is a bool type datatype if we want to test something and return type is
+// true or false than we use it
+  dynamic a = 10;
+  print("is operator:${a is double}");
+  // is! operator
+  // this is opposite of is operator
+  var x = "noushad";
+  print("is! operator:${x is! int}");
+}
