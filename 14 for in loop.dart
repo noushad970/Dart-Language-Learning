a2f012034b35e7d388e75641c3cd9f932402e8e5
MD. NOUSHAD BHUIYAN <142130428+noushad970@users.noreply.github.com>
2023-08-28 22:32:56 +0600

    Add files via upload

---
+++ 14 for in loop.dart
@@ -0,0 +1,7 @@
+void main() {
+  var a = [12323, 1212, 34, 34343, 231323];
+  for (var x in a) print(x);
+}
+//for in loop is like a for loop
+//for(datatype new variable name in old variable)
+//print(new variable)
---
+++ 15.anomynous or lamnda function.dart
@@ -0,0 +1,10 @@
+void main() {
+  var myName = (var name) {
+    print(name);
+  };
+  Function myRoll = (var roll) {
+    print(roll);
+  };
+  myName("NOUSHAD");
+  myRoll("21");
+}
