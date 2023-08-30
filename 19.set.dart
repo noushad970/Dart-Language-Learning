7330a086e410a2671276dce0067d1a80a95024f6
MD. NOUSHAD BHUIYAN <142130428+noushad970@users.noreply.github.com>
2023-08-30 23:30:17 +0600

    Add files via upload

---
+++ 19.set.dart
@@ -0,0 +1,16 @@
+void main() {
+  Set s = {1, 23, 4, 5};
+  s.add(4);
+  s.add(23);
+  s.add('noushad');
+  print(s);
+  for (var i in s) print(i);
+}
+/*
+set is a dynamic list type method which is not accessable 
+in a sorted form and we can add any datatype of elements in set
+(such as int ,double,string).we have to
+add all the sets element inside a 2nd braket. 
+
+
+*/
